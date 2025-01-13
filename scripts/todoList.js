9cdeedebcdd3b367e3cb606bbe3072ef275314af
gguggurggu <sentmeroses121@gmail.com>
2025-01-13 17:19:13 +0900

    Make delete button interactive

--- scripts/todoList.js
+++ scripts/todoList.js
@@ -1,6 +1,33 @@
 export function addTodoList() {
+  const savedTodos = JSON.parse(localStorage.getItem("save-to-do")) || [];
   const inputElement = document.querySelector(".js-to-do-input-input");
   const todoListContainer = document.querySelector(".js-to-do-ul");
+
+  const createTodoItem = (todo) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `<input type="checkbox"> ${todo}
+                <button class="to-do-input-button to-do-delete-button js-to-do-delete-button">
+                  <img
+                    class="delete-button"
+                    src="/images/icons/delete-button.png"
+                  /> </button>`;
+
+    listItem
+      .querySelector(".js-to-do-delete-button")
+      .addEventListener("click", () => {
+        listItem.remove();
+        const index = savedTodos.indexOf(todo);
+        if (index !== -1) {
+          savedTodos.splice(index, 1);
+          localStorage.setItem("save-to-do", JSON.stringify(savedTodos));
+        }
+      });
+    return listItem;
+  };
+
+  savedTodos.forEach((todo) => {
+    todoListContainer.appendChild(createTodoItem(todo));
+  });
 
   document
     .querySelector(".js-to-do-input-button")
@@ -8,10 +35,9 @@ export function addTodoList() {
       const inputValue = inputElement.value.trim();
 
       if (inputValue) {
-        const listItem = document.createElement("li");
-        listItem.innerHTML = `<input type="checkbox"> ${inputValue}`;
-
-        todoListContainer.appendChild(listItem);
+        todoListContainer.appendChild(createTodoItem(inputValue));
+        savedTodos.push(inputValue);
+        localStorage.setItem("save-to-do", JSON.stringify(savedTodos));
 
         inputElement.value = "";
       }
